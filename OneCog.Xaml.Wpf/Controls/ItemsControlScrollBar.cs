c0792990910e40934cafc5173ee38ec1c169233c
Ian Bebbington <github@bebbs.co.uk>
2015-12-12 10:06:58 +0000

    Initial commit

---
+++ OneCog.Xaml.Wpf/Controls/ItemsControlScrollBar.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OneCog.Xaml.Controls
+{
+    /// <summary>
+    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+    ///
+    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:OneCog.Xaml.Controls"
+    ///
+    ///
+    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:OneCog.Xaml.Controls;assembly=OneCog.Xaml.Controls"
+    ///
+    /// You will also need to add a project reference from the project where the XAML file lives
+    /// to this project and Rebuild to avoid compilation errors:
+    ///
+    ///     Right click on the target project in the Solution Explorer and
+    ///     "Add Reference"->"Projects"->[Browse to and select this project]
+    ///
+    ///
+    /// Step 2)
+    /// Go ahead and use your control in the XAML file.
+    ///
+    ///     <MyNamespace:ItemsControlScrollBar/>
+    ///
+    /// </summary>
+    public class ItemsControlScrollBar : ScrollBar
+    {
+        public static readonly DependencyProperty ItemsControlProperty = DependencyProperty.Register("ItemsControl", typeof(ItemsControl), typeof(ItemsControlScrollBar), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty ItemTemplateProperty = DependencyProperty.Register("ItemTemplate", typeof(DataTemplate), typeof(ItemsControlScrollBar), new PropertyMetadata(null));
+
+        static ItemsControlScrollBar()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ItemsControlScrollBar), new FrameworkPropertyMetadata(typeof(ItemsControlScrollBar)));
+        }
+        
+        public ItemsControl ItemsControl
+        {
+            get { return (ItemsControl)GetValue(ItemsControlProperty); }
+            set { SetValue(ItemsControlProperty, value); }
+        }
+
+        public DataTemplate ItemTemplate
+        {
+            get { return (DataTemplate)GetValue(ItemTemplateProperty); }
+            set { SetValue(ItemTemplateProperty, value); }
+        }        
+    }
+}
